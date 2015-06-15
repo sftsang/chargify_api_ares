7f9cac6974d87460652505ce63f9880722e619c9
Sherman Tsang <sherman@e19.ca>
2015-06-15 13:43:16 -0600

    Update usage.rb

--- lib/chargify_api_ares/resources/usage.rb
+++ lib/chargify_api_ares/resources/usage.rb
@@ -1,5 +1,7 @@
 module Chargify
   class Usage < Base
+    self.prefix = "/subscriptions/:subscription_id/components/:component_id/"
+    
     def subscription_id=(i)
       self.prefix_options[:subscription_id] = i
     end
